bc881ea904b8dc430ca8051bce1522f096cca65d
Jannis Liebscher <jannis-liebscher@gmx.de>
2021-11-25 15:39:46 +0100

    main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,23 +2,8 @@ import scala.io.StdIn.readLine
 
 object shutthebox {
   @main def run: Unit =
-    /* println("Welcome!")
-    println("Enter HP between 0 and 10")
-    val health = readLine.toInt
-    val mesh = (bar(width) + cells(width)) * zeilen + bar(width)
-    println(mesh)
-    print("HP:" + healthbar(health) + "\n") */
     val c =
       new controller.Controller(new model.Board(9), new model.Dice())
     val tui = new aview.Tui(c)
     tui.run
-
-  val width = 1
-  val zeilen = 1
-  val spalten = 20
-  def cells(cellwidth: Int = 3) = ("|" + " " * cellwidth) * spalten + "|\n"
-  def bar(cellwidth: Int = 3) = ("+" + "-" * cellwidth) * spalten + "+\n"
-
-  def healthbar(hp: Int = 0): String =
-    ("|" + "+ " * hp) + "  " * (10 - hp) + "|"
 }
