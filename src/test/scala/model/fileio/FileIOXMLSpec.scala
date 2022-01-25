c6d1938487582a48ddecf14a9f24bc028ce6b6cb
Jannis Liebscher <jannis-liebscher@gmx.de>
2022-01-25 16:51:52 +0100

    xmlSpec

--- src/test/scala/model/fileio/FileIOXMLSpec.scala
+++ src/test/scala/model/fileio/FileIOXMLSpec.scala
@@ -1,5 +1,13 @@
 package model.fileio
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+import scala.util.control.Breaks._
+import model.Game
+import model.fileioComponent.FileIOJSON
+import model.*
+class FileIOXMLSpec extends AnyWordSpec {
 
-class FileIOXMLSpec {
-  
+  "A FileIOXML" should {
+    "save a test game \"game.json\"" in {}
+  }
 }
