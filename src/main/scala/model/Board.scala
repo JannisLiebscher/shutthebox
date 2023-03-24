c0f8131346d984ea9cb1007668a47a9666811204
TobiasL98 <latt.tobias@web.de>
2023-03-24 15:11:00 +0100

    New Branch

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -4,28 +4,45 @@ case class Board private (var m: Matrix[Int]) extends BoardInterface {
 
   def this(size: Int = 9) =
     this(new Matrix[Int](size, 0))
-    for (n <- (0 to m.size - 1)) m = m.replace(n, 0, n + 1)
+
+  for (n <- (0 to m.size - 1)) m = m.replace(n, 0, n + 1)
 
   def shut(num: Int): Board =
     new Board(m.replace(num - 1, 0, 0).replace(num - 1, 1, num))
+
   def resShut(num: Int): Board =
     new Board(m.replace(num - 1, 0, num).replace(num - 1, 1, 0))
+
   def isShut(num: Int): Boolean = m.cell(num - 1, 0) == 0
 
   def count(): Int =
-    var sum = 0
-    for (n <- (0 to m.size - 1)) sum += m.cell(n, 0)
-    return sum
 
-  override def toString(): String =
-    var out = "| "
-    for (n <- (1 to m.size))
-      if (m.cell(n - 1, 0) != 0) out = out + n + " | "
-      else out = out + "# | "
-    out = out + sys.props("line.separator") + "| "
-    for (n <- (1 to m.size)) {
-      if (m.cell(n - 1, 1) != 0) out = out + n + " | "
-      else out = out + "# | "
-    }
-    return out
+  var sum = 0
+  for (n <- (0 to m.size - 1)) sum += m.cell(n, 0)
+  return sum
+
+  //  override def toString(): String =
+  //    var out = "| "
+  //    for (n <- (1 to m.size))
+  //      if (m.cell(n - 1, 0) != 0) out = out + n + " | "
+  //      else out = out + "# | "
+  //    out = out + sys.props("line.separator") + "| "
+  //    for (n <- (1 to m.size)) {
+  //      if (m.cell(n - 1, 1) != 0) out = out + n + " | "
+  //      else out = out + "# | "
+  //    }
+  //    return out
+  //}
 }
+  override def toString(): String = {
+    val firstRow = (1 to m.size)
+      .map(n => if (m.cell(n - 1, 0) != 0) s"$n | " else "# | ")
+      .mkString("| ", "", sys.props("line.separator") + "| ")
+
+    val secondRow = (1 to m.size)
+      .map(n => if (m.cell(n - 1, 1) != 0) s"$n | " else "# | ")
+      .mkString("| ", "", "")
+
+    s"$firstRow$secondRow"
+  }
+
