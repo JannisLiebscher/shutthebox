798b4857146926cb0272c7ad44321bd0b930decc
Jannis Liebscher <jannis-liebscher@gmx.de>
2023-03-25 14:26:22 +0100

    removed var in JSON FileIO

--- src/main/scala/model/fileioComponent/FileIOJSON.scala
+++ src/main/scala/model/fileioComponent/FileIOJSON.scala
@@ -7,30 +7,24 @@ import scala.io.Source
 class FileIOJSON extends FileIOInterface {
 
   override def load: GameInterface = {
-    var board: BoardInterface = new Board()
-    var playersSeq = Seq[Int]()
     val source: String = Source.fromFile("game.json").getLines.mkString
     val json: JsValue = Json.parse(source)
+
     val sum = (json \ "game" \ "sum").get.toString.toInt
-    playersSeq = playersSeq
-      :+ (json \ "game" \ "players" \ "score1").get.toString.toInt
-      :+ (json \ "game" \ "players" \ "score2").get.toString.toInt
-      :+ (json \ "game" \ "players" \ "turn").get.toString.toInt
-    var players = new Players(
+    val playersSeq = Seq[Int](
+      (json \ "game" \ "players" \ "score1").get.toString.toInt,
+      (json \ "game" \ "players" \ "score2").get.toString.toInt,
+      (json \ "game" \ "players" \ "turn").get.toString.toInt
+    )
+    val players = new Players(
       2,
       Vector(("Player 1", playersSeq(0)), ("Player 2", playersSeq(1))),
       playersSeq(2)
     )
-    // val boardstate = (json \ "game" \ "board").get.toString.split(",")
-    // boardstate(0) = boardstate(0).slice(1, 30)
-    // boardstate(boardstate.length - 1) = boardstate(boardstate.length - 1)
-    //   .slice(0, boardstate(boardstate.length - 1).length - 1)
-
-    // for (i <- (0 to 8))
-    //   if (boardstate(i).equals("true")) board = board.shut(i + 1)
     val boardstate = (json \ "game" \\ "board")(0).asInstanceOf[JsArray]
-    for (i <- (0 to 8))
-      if (boardstate(i).toString.toBoolean) board = board.shut(i + 1)
+    val board: BoardInterface = (0 to 8)
+      .filter(i => boardstate(i).toString.toBoolean)
+      .foldLeft(new Board(): BoardInterface)((acc, i) => acc.shut(i + 1))
     return new Game(board, Dice("two"), players, sum)
   }
 
@@ -50,9 +44,7 @@ class FileIOJSON extends FileIOInterface {
     )
   }
   def boardToJson(game: GameInterface): Seq[Boolean] = {
-    var seq = Seq[Boolean]()
-    for (i <- (1 to 9)) seq = seq :+ game.isShut(i)
-    return seq
+    (1 to 9).map(x => game.isShut(x)).toSeq
   }
   def playersToJson(game: GameInterface) = {
     Json.obj(
